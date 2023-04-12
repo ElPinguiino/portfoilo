6b522f18d2f1e3cc5b0dede5b9afa2da7d316500
Juan Aguilera <64390726+ElPinguiino@users.noreply.github.com>
2023-04-12 01:08:26 -0600

    integrated openai and made some ui changes

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <section>
-        <h5>Nice to meet you, my name is Juan. I am a passionate Software Developer with a background in IT Support who has experience building web applications in a variety of Frameworks, Libraries, and Languages. Currently, I enjoy using React for the Frontend and Django for the backend but, love to build things in with whatever tools are most appropiate for the job.</h5>
+    <section id="about">
+        <h5>Hi there! It's a pleasure to meet you. My name is Juan, and I'm a passionate Software Developer with a background in IT Support. I have experience building web applications using various frameworks, libraries, and languages. Currently, I'm enjoying working with React for the frontend and Django for the backend, but I'm always open to using the most appropriate tools for the job. 
+        </h5>
+        <h5>
+        I believe that creating user-friendly and efficient solutions is key to the success of any software application. I'm a collaborative team player who values effective communication and mutual respect. I'm also committed to continuous learning to stay up-to-date with the latest industry trends.
+        </h5>
     </section>
   )
 }
---
+++ portfolio/src/components/Chat.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+
+const Chat = () => {
+
+    const [gptResponse, setGptResponse] = useState([])
+    const [message, setMessage] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios({
+      method: 'POST',
+      url: 'https://openai80.p.rapidapi.com/chat/completions',
+      headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': '100fe98584msh3cc3759f9aead09p18070ajsnfd70377b7ae1',
+        'X-RapidAPI-Host': 'openai80.p.rapidapi.com'
+      },
+      data: `{"model":"gpt-3.5-turbo","messages":[{"role":"user","content":"${message}"}]}`
+    }).then((response) => {
+      console.log(response.data);
+      console.log(message)
+      console.log(response.data.choices[0].message.content)
+      setGptResponse(response.data.choices[0].message.content)
+    }).catch(error => console.error(error));
+  }
+
+  return (
+    <section className="chat-main-container">
+      <article>
+      <form>
+      <h1>What is your question?</h1>
+      <textarea
+        defaultValue={message} 
+        onChange={(e) => setMessage(e.target.value)}
+      ></textarea>
+       <br />
+      <button 
+        onClick={handleSubmit}
+      >
+        Submit
+      </button>
+      </form>
+      </article>
+      <article>
+        <p>{gptResponse}</p>
+      </article>
+    </section>
+  )
+}
+
+export default Chat
